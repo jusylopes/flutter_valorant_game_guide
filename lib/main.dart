8ebd901027dd9550f4ac48db234fbf8abf37cebe
jusylopes <jusylopes@outlook.com>
2023-05-21 22:17:51 -0300

    refactor: removing folders

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Valorant Game Guide',
+      title: 'Valorant Guide',
     );
   }
 }
