51a36e382a459dac9c71e48062756a154afbc4b8
jusylopes <jusylopes@outlook.com>
2023-05-31 14:24:54 -0300

    refactor: repository

--- lib/repositories/repository.dart
+++ lib/repositories/repository.dart
@@ -3,9 +3,11 @@ import 'package:flutter_valorant_game_guide/models/agent.dart';
 import 'package:flutter_valorant_game_guide/repositories/adapters/agent_adapter.dart';
 
 class Repository {
-  Repository({required this.dio});
+  Repository({required this.dio, required this.agentAdapter});
 
   final Dio dio;
+  final AgentAdapter agentAdapter;
+
   static const String _baseApi = 'https://valorant-api.com/v1';
 
   Future<List<Agent>> getAgents() async {
@@ -13,7 +15,7 @@ class Repository {
       Response response = await dio.get('$_baseApi/agents');
       final data = response.data;
 
-      return AgentAdapter.fromJson(data);
+      return agentAdapter.fromJson(data);
     } catch (e) {
       rethrow;
     }
