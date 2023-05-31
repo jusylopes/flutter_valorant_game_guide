f3a675438f107aaf17a2b87cba3e415ba21fedd3
jusylopes <jusylopes@outlook.com>
2023-05-31 14:25:15 -0300

    add: agent adapter

--- lib/repositories/adapters/agent_adapter.dart
+++ lib/repositories/adapters/agent_adapter.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_valorant_game_guide/models/agent.dart';
 
-abstract class AgentAdapter {
-  static List<Agent> fromJson(dynamic json) {
+abstract class IAgentAdapter {
+  List<Agent> fromJson(dynamic json);
+}
+
+class AgentAdapter extends IAgentAdapter {
+  @override
+  List<Agent> fromJson(dynamic json) {
     return json['data'].map<Agent>((data) => Agent.fromJson(data)).toList();
   }
 }
