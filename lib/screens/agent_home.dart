be256b6ba3ac20a65db65e2fb7fd50f1ce22fb09
jusylopes <jusylopes@outlook.com>
2023-06-09 21:06:14 -0300

    add: home screen

---
+++ lib/screens/agent_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AgentHome extends StatelessWidget {
+  const AgentHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
