cf8ccb0b703734bdb8bd797233d113a074698916
jusylopes <jusylopes@outlook.com>
2023-06-09 21:06:03 -0300

    add: detail screen

---
+++ lib/screens/agent_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AgentDetail extends StatelessWidget {
+  const AgentDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
