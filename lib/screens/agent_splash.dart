c3b2965eb88a4121b9cc3f232dea3dece83ef7c4
jusylopes <jusylopes@outlook.com>
2023-06-13 19:18:09 -0300

    refactor: agent splash

--- lib/screens/agent_splash.dart
+++ lib/screens/agent_splash.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_valorant_game_guide/resources/colors.dart';
 
-class AgentSplash extends StatelessWidget {
+class AgentSplash extends StatefulWidget {
   const AgentSplash({super.key});
 
   @override
+  State<AgentSplash> createState() => _AgentSplashState();
+}
+
+class _AgentSplashState extends State<AgentSplash>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: ValorantColors.primaryColor,
+      body: Center(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
