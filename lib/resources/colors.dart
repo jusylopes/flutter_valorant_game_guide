b2c53579b0396853b37fd9f214fcaba46ede5d08
jusylopes <jusylopes@outlook.com>
2023-06-13 19:17:52 -0300

    add: class colors

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
 class ValorantColors{
- 
+ static const Color primaryColor = Color(0xFF0F1923);
+  static const Color secondaryColor = Color(0xFFFF4655);
 }
