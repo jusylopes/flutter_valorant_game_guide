6e46c6180689312f27281576bc63541b55a6d1af
jusylopes <jusylopes@outlook.com>
2023-06-19 09:43:32 -0300

    refactor: add animation splash

--- lib/screens/agent_splash.dart
+++ lib/screens/agent_splash.dart
@@ -11,12 +11,39 @@ class AgentSplash extends StatefulWidget {
 class _AgentSplashState extends State<AgentSplash>
     with TickerProviderStateMixin {
   late final AnimationController _controller;
+  late final Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat();
+
+    _animation = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(0, -0.5),
+    ).animate(_controller);
+
+    //_controller.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: ValorantColors.primaryColor,
-      body: Center(),
+      body: Stack(
+        children: [
+          const Positioned.fill(
+            child: FlutterLogo(size: 200),
+          ),
+          SlideTransition(
+            position: _animation,
+            child:Image.asset('assets/images/valorant-logo.png'),
+          ),
+        ],
+      ),
     );
   }
 
