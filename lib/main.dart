a4e1d61ad6fe3541206c35536f7985c6f2c88b52
jusylopes <jusylopes@outlook.com>
2023-06-25 22:58:15 -0300

    add: agent detail page

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_valorant_game_guide/repositories/adapters/agent_adapter.
 import 'package:flutter_valorant_game_guide/repositories/repository.dart';
 import 'package:flutter_valorant_game_guide/resources/strings.dart';
 import 'package:flutter_valorant_game_guide/resources/theme.dart';
-import 'package:flutter_valorant_game_guide/screens/agent_splash.dart';
+import 'package:flutter_valorant_game_guide/views/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         theme: ValorantTheme.dark,
         debugShowCheckedModeBanner: false,
         title: ValorantStrings.appName,
-        home: const AgentSplash(),
+        home: const SplashPage(),
       ),
     );
   }
--- lib/views/agent_detail_page.dart
+++ lib/views/agent_detail_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AgentDetail extends StatelessWidget {
-  const AgentDetail({super.key});
+class AgentDetailPage extends StatelessWidget {
+  const AgentDetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
