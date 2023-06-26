80b03eb9ee1ab861e632e6cabad26824f74cdb8d
jusylopes <jusylopes@outlook.com>
2023-06-25 22:58:02 -0300

    add: agent page

---
+++ lib/views/agents_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AgentsPage extends StatelessWidget {
+  const AgentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
