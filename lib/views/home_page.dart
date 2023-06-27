b93c6d66bd1b7bae2054f9f5d29863afe4282014
jusylopes <jusylopes@outlook.com>
2023-06-26 22:12:58 -0300

    refactor: home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -6,10 +6,16 @@ import 'package:flutter_valorant_game_guide/views/agents_page.dart';
 class AgentHomePage extends StatelessWidget {
   const AgentHomePage({super.key});
 
+  static const List<Tab> myTabs = <Tab>[
+    Tab(child: Text('Agents')),
+    Tab(child: Text('Weapons')),
+    Tab(child: Text('Maps')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
           title: Column(
@@ -30,14 +36,18 @@ class AgentHomePage extends StatelessWidget {
           bottom: TabBar(
               labelStyle: TextStyle(
                   fontSize: 18.0, fontFamily: ValorantStrings.productSansFont),
-              indicatorColor: ValorantColors.secondaryColor,
-              tabs: const [
-                Tab(child: Text('Agents')),
-                Tab(child: Text('Weapons')),
-                Tab(child: Text('Maps')),
-              ]),
+              indicator: const UnderlineTabIndicator(
+                borderSide: BorderSide(
+                    width: 3.0, color: ValorantColors.secondaryColor),
+                insets: EdgeInsets.symmetric(horizontal: 50.0),
+              ),
+              unselectedLabelColor: Colors.grey,
+              
+              tabs: myTabs),
         ),
-        body: const TabBarView(children: [AgentsPage()]),
+        body: const TabBarView(children: [
+          AgentsPage(),
+        ]),
       ),
     );
   }
