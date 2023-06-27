eef3337ad7ba4b2cb572545b8ee1418ad1c4fa8f
jusylopes <jusylopes@outlook.com>
2023-06-27 19:38:06 -0300

    refactor: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_valorant_game_guide/blocs/agent/agent_bloc.dart';
-import 'package:flutter_valorant_game_guide/blocs/agent/agent_event.dart';
-import 'package:flutter_valorant_game_guide/repositories/adapters/agent_adapter.dart';
-import 'package:flutter_valorant_game_guide/repositories/repository.dart';
-import 'package:flutter_valorant_game_guide/resources/strings.dart';
-import 'package:flutter_valorant_game_guide/resources/theme.dart';
-import 'package:flutter_valorant_game_guide/views/splash_page.dart';
+import 'package:flutter_valorant_game_guide/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final agentRepository =
-        Repository(dio: Dio(), agentAdapter: AgentAdapter());
-
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AgentBloc>(
-          create: (_) =>
-              AgentBloc(repository: agentRepository)..add(AgentLoaded()),
-        ),
-      ],
-      child: MaterialApp(
-        theme: ValorantTheme.dark,
-        debugShowCheckedModeBanner: false,
-        title: ValorantStrings.appName,
-        home: const SplashPage(),
-      ),
-    );
-  }
+  runApp(const App());
 }
