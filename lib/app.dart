565d965d0dbbe185b1134f4d78f0c219f53c1c7d
jusylopes <jusylopes@outlook.com>
2023-06-27 19:38:29 -0300

    add: app

---
+++ lib/app.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_valorant_game_guide/blocs/agent/agent_bloc.dart';
+import 'package:flutter_valorant_game_guide/blocs/agent/agent_event.dart';
+import 'package:flutter_valorant_game_guide/repositories/adapters/agent_adapter.dart';
+import 'package:flutter_valorant_game_guide/repositories/repository.dart';
+import 'package:flutter_valorant_game_guide/resources/strings.dart';
+import 'package:flutter_valorant_game_guide/resources/theme.dart';
+import 'package:flutter_valorant_game_guide/screens/splash_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final agentRepository =
+        Repository(dio: Dio(), agentAdapter: AgentAdapter());
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AgentBloc>(
+          create: (_) =>
+              AgentBloc(repository: agentRepository)..add(AgentLoaded()),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ValorantTheme.dark,
+        debugShowCheckedModeBanner: false,
+        title: ValorantStrings.appName,
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
