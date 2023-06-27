73d39abc39fa90730fc9eacb92010bac25646d23
jusylopes <jusylopes@outlook.com>
2023-06-26 21:40:51 -0300

    refactor: theme

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,4 +1,7 @@
 class ValorantStrings {
   static const appName = 'Valorant Guide';
   static const errorMessage = 'erro';
+
+  static String productSansFont = 'ProductSans';  
+  static String rubikFont = 'Rubik';
 }
--- lib/resources/theme.dart
+++ lib/resources/theme.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_valorant_game_guide/resources/colors.dart';
+import 'package:flutter_valorant_game_guide/resources/strings.dart';
 
 class ValorantTheme {
   static ThemeData get dark => ThemeData(
+      fontFamily: ValorantStrings.rubikFont,
       visualDensity: VisualDensity.adaptivePlatformDensity,
       brightness: Brightness.dark,
+      scaffoldBackgroundColor: ValorantColors.primaryColor,
       appBarTheme: const AppBarTheme(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        toolbarHeight: 80,
-        elevation: 0,
-      ),
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        selectedItemColor: Colors.white,
+          backgroundColor: ValorantColors.primaryColor,
+          centerTitle: true,
+          toolbarHeight: 100,
+          elevation: 0,
+          titleTextStyle: TextStyle(fontSize: 28.0)),
+      tabBarTheme: const TabBarTheme(
+        labelColor: ValorantColors.secondaryColor,
+        unselectedLabelColor: Colors.grey,
       ));
 }
