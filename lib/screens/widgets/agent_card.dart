23f5c169623c22f387c6382896845f15eaa16f5e
Jusy Lopes <jusylopes@outlook.com>
2023-07-01 21:09:10 -0300

    refactor: agent card

--- lib/screens/widgets/agent_card.dart
+++ lib/screens/widgets/agent_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_valorant_game_guide/models/agent.dart';
 import 'package:flutter_valorant_game_guide/resources/colors.dart';
 import 'package:flutter_valorant_game_guide/resources/strings.dart';
+import 'package:flutter_valorant_game_guide/screens/agent_detail_screen.dart';
+import 'package:flutter_valorant_game_guide/screens/widgets/agent_cached_network_image.dart';
 
 class AgentCard extends StatelessWidget {
   const AgentCard({super.key, required this.agent});
@@ -10,55 +12,59 @@ class AgentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.bottomStart,
-      children: [
-        Container(
-          height: 180,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-            color: ValorantColors.primaryColorBackground,
-              image: const DecorationImage(
-                image: AssetImage('images/veg.jpg'),
-                fit: BoxFit.cover,
-              )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 50),
-          child: SizedBox(
-            height: 250,
-            child: agent.bustPortrait != null
-                ? Image.network(
-                    agent.bustPortrait!,
-                    fit: BoxFit.cover,
-                  )
-                : Image.asset(ValorantStrings.appLogo),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AgentDetailScreen(agent: agent),
           ),
-        ),
-        Positioned(
-          left: 10,
-          bottom: 20,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                agent.role != null ? agent.role!.displayName : "",
-                style: TextStyle(
-                  fontSize: 12,
-                  fontFamily: ValorantStrings.rubikFont,
-                ),
-              ),
-              Text(
-                agent.displayName.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 22,
-                  fontFamily: ValorantStrings.rubikFont,
-                ),
-              ),
-            ],
+        );
+      },
+      child: Stack(
+        alignment: AlignmentDirectional.bottomStart,
+        children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: ValorantColors.primaryColorBackground,
+            ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(left: 45),
+            child: Center(
+              child: AgentCachedNetworkImage(
+                agent: agent,
+                sizeImage: 250,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 15,
+            bottom: 25,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  agent.role != null ? agent.role!.displayName : "",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: ValorantStrings.rubikFont,
+                      color: Colors.grey),
+                ),
+                Text(
+                  agent.displayName.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: ValorantStrings.rubikFont,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
