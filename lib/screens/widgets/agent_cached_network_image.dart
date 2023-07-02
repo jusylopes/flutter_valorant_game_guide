824ec494cba11970eb5477264256580d3e94ba1e
Jusy Lopes <jusylopes@outlook.com>
2023-07-01 21:07:53 -0300

    add: agent image

---
+++ lib/screens/widgets/agent_cached_network_image.dart
@@ -0,0 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_valorant_game_guide/models/agent.dart';
+import 'package:flutter_valorant_game_guide/resources/strings.dart';
+import 'package:flutter_valorant_game_guide/screens/widgets/agent_circular_progress_indicator.dart';
+
+class AgentCachedNetworkImage extends StatelessWidget {
+  const AgentCachedNetworkImage({
+    super.key,
+    required this.agent, required this.sizeImage,
+  });
+
+  final Agent agent;
+   final double sizeImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: agent.uuid,
+      child: CachedNetworkImage(
+          alignment: Alignment.bottomRight,
+          imageUrl: agent.fullPortrait ?? ValorantStrings.noImage,
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              const AgentCircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          imageBuilder: (context, imageProvider) {
+            return Container(
+                height: sizeImage,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ));
+          }),
+    );
+  }
+}
