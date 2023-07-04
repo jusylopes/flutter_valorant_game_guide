5e213f4c22cc78be030749c74f2826513ca0fc23
Jusy Lopes <jusylopes@outlook.com>
2023-07-04 00:16:16 -0300

    refactor: agent image

--- lib/screens/widgets/agent_cached_network_image.dart
+++ lib/screens/widgets/agent_cached_network_image.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_valorant_game_guide/models/agent.dart';
-import 'package:flutter_valorant_game_guide/resources/strings.dart';
 import 'package:flutter_valorant_game_guide/screens/widgets/agent_circular_progress_indicator.dart';
 
 class AgentCachedNetworkImage extends StatelessWidget {
@@ -18,26 +17,26 @@ class AgentCachedNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      alignment: Alignment.bottomRight,
-      imageUrl: agent.fullPortrait ?? '',
-      progressIndicatorBuilder: (context, url, downloadProgress) =>
-          const AgentCircularProgressIndicator(),
-      errorWidget: (context, url, error) => const Icon(Icons.error),
-      imageBuilder: (context, imageProvider) {
-        return Padding(
-          padding: paddingImage,
-          child: Container(
-            height: sizeImage,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        );
-      },
-    );
+    return  CachedNetworkImage(
+            alignment: Alignment.bottomRight,
+            imageUrl: agent.fullPortrait ?? '',
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                const AgentCircularProgressIndicator(),
+            imageBuilder: (context, imageProvider) {
+              return Padding(
+                padding: paddingImage,
+                child: Container(
+                  height: sizeImage,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+      
   }
 }
